3092b0e589a33758f8373e68c1621e37dd1e12eb
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-10-03 19:31:29 -0700

    Added tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,17 @@
-const { readPhoneNumber } = require("../dist/index");
+const { formatPhoneNumber } = require("../dist/index");
 
 describe("readPhoneNumber", () => {
+  it("should convert a ten digit string into a human readable format", () => {
+    expect(formatPhoneNumber("1234567890")).toEqual("(123) 456-7890");
+  });
+
   it("should convert a ten digit number into a human readable format", () => {
-    expect(readPhoneNumber("1234567890")).toEqual("(123) 456-7890");
+    expect(formatPhoneNumber(1234567890)).toEqual("(123) 456-7890");
+  });
+
+  it("should throw an error if the input is not a string or number of ten digits", () => {
+    expect(() => formatPhoneNumber({})).toThrow(
+      "Phone number must be 10 digits"
+    );
   });
 });
